5d09e126135b3b58eac12837e51655bf9ab9e966
fethallah <fethallahbenmokhtar@gmail.com>
2024-08-31 18:20:03 +0100

    new

---
+++ guessing_game/target/debug/deps/guessing_game-4087b4b4b3dce924.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\rustl\rust-projects\guessing_game\target\debug\deps\libguessing_game-4087b4b4b3dce924.rmeta: src/main.rs
+
+C:\Users\DELL\rustl\rust-projects\guessing_game\target\debug\deps\guessing_game-4087b4b4b3dce924.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-535155d9402c860e.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\rustl\rust-projects\guessing_game\target\debug\deps\libguessing_game-535155d9402c860e.rmeta: src/main.rs
+
+C:\Users\DELL\rustl\rust-projects\guessing_game\target\debug\deps\guessing_game-535155d9402c860e.d: src/main.rs
+
+src/main.rs:
